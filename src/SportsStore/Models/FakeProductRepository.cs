a5c46c598d69bab5da6c0c335c5fb9dfe7390440
Dmitry Terlikov <dmitry.terlikov@sap.com>
2018-09-02 12:30:16 +0400

    First git commit.

--- src/SportsStore/Models/FakeProductRepository.cs
+++ src/SportsStore/Models/FakeProductRepository.cs
@@ -8,9 +8,9 @@ namespace SportsStore.Models
     public class FakeProductRepository : IProductRepository
     {
         public IEnumerable<Product> Products => new List<Product> {
-            new Product { Name = "Football", Price = 25 },
-            new Product { Name = "Surf board", Price = 179 },
-            new Product { Name = "Running shoes", Price = 95, Description = "Boots" }
+            new Product { Name = "Football", Price = 250 },
+            new Product { Name = "Surf board", Price = 1790 },
+            new Product { Name = "Running shoes", Price = 950, Description = "Boots" }
         };
     }
 }
