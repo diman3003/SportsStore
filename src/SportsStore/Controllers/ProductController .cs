d8e65ab9982ea9cd492cb2da645fcb1dfcc2278c
Dmitry Terlikov <dmitry.terlikov@sap.com>
2018-09-02 21:21:21 +0400

    Add test and moq.

--- src/SportsStore/Controllers/ProductController .cs
+++ src/SportsStore/Controllers/ProductController .cs
@@ -1,20 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using SportsStore.Models;
+﻿using SportsStore.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace SportsStore.Controllers
 {
     public class ProductController : Controller
     {
         private IProductRepository repository;
+        public int pageSize = 4;
+
         public ProductController(IProductRepository repo)
         {
             repository = repo;
         }
 
-        public ViewResult List() => View(repository.Products);
+        public ViewResult List(int page = 1) => View(repository
+                                                .Products.OrderBy(p => p.ProductID)
+                                                .Skip((page -1) * pageSize)
+                                                .Take(pageSize));
     }
 }
